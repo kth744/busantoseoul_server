f776e96877dd83ae7a757acbbe86a0caeef7583f
thkim <kth744@gmail.com>
2016-06-16 02:07:01 +0900

    init commit

---
+++ src/main/java/com/busantoseoul/BusantoseoulServerApplication.java
@@ -0,0 +1,12 @@
+package com.busantoseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusantoseoulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusantoseoulServerApplication.class, args);
+	}
+}
